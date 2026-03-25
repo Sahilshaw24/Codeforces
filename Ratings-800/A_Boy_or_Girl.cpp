c08cc42e93d4b7decc733df367613ca85d1e8fa4
Sahil Shaw <shaws2857@gmail.com>
2026-03-25 20:57:19 +0530

    solved

--- Ratings-800/A_Boy_or_Girl.cpp
+++ Ratings-800/A_Boy_or_Girl.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main(){
     string s;
     cin >> s;
-    set<int> stt;
+    set<int> st;
     for(auto it : s){
         stt.insert(it);
     }
-    if((stt.size()) % 2 == 0) cout << "CHAT WITH HER!";
+    if((st.size()) % 2 == 0) cout << "CHAT WITH HER!";
     else cout << "IGNORE HIM!";
     return 0;
 }
