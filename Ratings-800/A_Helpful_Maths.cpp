3d62b79f9c1cfa5c802958818aaf3aa50842f5fe
Sahil Shaw <shaws2857@gmail.com>
2026-03-25 21:53:24 +0530

    solved

---
+++ Ratings-800/A_Helpful_Maths.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    string s;
+    cin >> s;
+    string s2;
+    for(char c : s){
+        if(c != '+'){
+            s2 = s2 + c;
+        }
+    }
+    sort(s2.begin(),s2.end());
+    string ans;
+    for(char c : s2){
+        ans = ans + c;
+        if(ans.size() != s.size()) ans = ans + "+";
+    }
+    cout << ans;
+    return 0;
+}
