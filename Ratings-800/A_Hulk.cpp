b714b9a89d04059d094113c6e37232dad7a7352c
Sahil Shaw <shaws2857@gmail.com>
2026-04-07 22:05:35 +0530

    solved

---
+++ Ratings-800/A_Hulk.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    
+    string s;
+    for(int i=1;i<=n;i++){
+        if(i % 2 == 0) s += "I love ";
+        else s += "I hate ";
+        
+        if(i != n) s += "that ";
+    }
+    cout << s + "it";
+    return 0;
+}
