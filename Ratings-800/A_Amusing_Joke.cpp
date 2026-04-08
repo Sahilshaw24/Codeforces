6c5d194ca6de9505b6a3df42e543ca563d86995b
Sahil Shaw <shaws2857@gmail.com>
2026-04-08 20:06:06 +0530

    solved

---
+++ Ratings-800/A_Amusing_Joke.cpp
@@ -0,0 +1,18 @@
+// Problem link: https://codeforces.com/problemset/problem/141/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string s1,s2,s3;
+    cin >> s1 >> s2 >> s3;
+    s1 = s1 + s2;
+    sort(s1.begin(),s1.end());
+    sort(s3.begin(),s3.end());
+    if(s1 == s3) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
