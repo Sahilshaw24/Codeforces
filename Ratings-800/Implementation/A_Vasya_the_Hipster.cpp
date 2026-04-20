e4f71c894c72b2076bae0645ea1015d87b0c889f
Sahil Shaw <shaws2857@gmail.com>
2026-04-20 22:27:00 +0530

    solved

---
+++ Ratings-800/Implementation/A_Vasya_the_Hipster.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int red,blue;
+    cin >> red >> blue;
+    
+    int diffPair = 0, samePair = 0;
+    while(true){
+        if(red && blue){
+            diffPair++;
+            red--;
+            blue--;
+        }
+        else if(red > 1){
+            samePair++;
+            red -= 2;
+        }
+        else if(blue > 1){
+            samePair++;
+            blue -= 2;
+        }
+        else break;
+    }
+    
+    cout << diffPair << " " << samePair;
+    return 0;
+}
